1c00f4d1d059642c430813fdc59cd79c6a0791ba
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-05-08 07:27:16 +0700

    📦 Add raw asset

---
+++ raw/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tilemap" tilewidth="16" tileheight="14" tilecount="256" columns="16">
+ <image source="../assets/tilemap.png" width="256" height="224"/>
+</tileset>
