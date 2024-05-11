22a8d6c6c22519b0590d51a4d7d087505ad51313
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-05-11 12:18:01 +0700

    🛠️ Bugfix on HUD

---
+++ src/HUD.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var hp = get_parent().get_node("Player").get_node("Health")
+@onready var hp_bar = get_node("hp_bar")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func update_ui():
+	hp_bar.text = "[right]" + str(hp.hp) + "[/right]"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update_ui()
