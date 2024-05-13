d11178596b1d0e8a9d0a40875ad6b44a625a03a9
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-05-13 19:51:13 +0700

    🛠️ fix non moving platform

--- src/moving_platform.gd
+++ src/moving_platform.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
 @export var offset = Vector2(0, -320)
-@export var duration = 1.
+@export var duration = 5.
 var tween
 
 func _ready():
-	start_tween()
-
-func start_tween():
-	tween = get_tree().create_tween().bind_node(self).set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.set_loops()
-	tween.tween_property($AnimatableBody2d, "position", offset, duration / 2)
-	tween.tween_property($AnimatableBody2d, "position", Vector2.ZERO, duration / 2)
+	tween = get_tree().create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	tween.set_loops().set_parallel(false)
+	tween.tween_property($AnimatableBody2D, "position", offset, duration / 2)
+	tween.tween_property($AnimatableBody2D, "position", Vector2.ZERO, duration / 2)
