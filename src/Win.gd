3901df73ed30e7274846c5ea758aba0f1f001fe7
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-05-21 17:21:56 +0700

    📦 Addded bgm for win

--- src/Win.gd
+++ src/Win.gd
@@ -1,10 +1,12 @@
 extends Control
 
+@onready var win_bgm = preload("res://assets/bgm/win.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer2.play("credits")
 	DiscordPresence.set_detail("In Credits")
+	SoundChip.play_music(win_bgm)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
